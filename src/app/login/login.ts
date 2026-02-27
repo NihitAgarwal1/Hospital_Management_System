a61e726f3c7f34f58c21262f7d690d96121d7227
Nihit Agarwal <nihitagarwal0@gmail.com>
2026-02-27 22:32:11 +0530

    Login completed 1

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+     name: string = '';
+    password: string = '';
+    constructor(private router: Router) {}
+
+  login() {
+    if (this.name && this.password) {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Please enter login details');
+    }
+  }
 
 }
