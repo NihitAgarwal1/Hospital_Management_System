0ebf2d3c292e879b04ee4c8d3351f76d46c3cfaf
Nihit Agarwal <nihitagarwal0@gmail.com>
2026-02-28 15:53:38 +0530

    Bill added1

---
+++ src/app/bill/bill.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-bill',
+  imports: [RouterLink],
+  templateUrl: './bill.html',
+  styleUrl: './bill.css',
+})
+export class Bill {
+
+}
